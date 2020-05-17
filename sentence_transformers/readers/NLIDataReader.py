eaea78249b72f2bf2ca58b38e1d4b53d6e206309
mithun <mithunpaul08@gmail.com>
2020-05-17 00:27:38 -0700

    more print statements for label

--- sentence_transformers/readers/NLIDataReader.py
+++ sentence_transformers/readers/NLIDataReader.py
@@ -2,7 +2,7 @@ from . import InputExample
 import csv
 import gzip
 import os
-
+import logging
 
 class NLIDataReader(object):
     """
@@ -12,6 +12,7 @@ class NLIDataReader(object):
         self.dataset_folder = dataset_folder
 
     def get_examples(self, filename, max_examples=0):
+        logging.info("inside get_examples")
         """
         data_splits specified which data split to use (train, dev, test).
         Expects that self.dataset_folder contains the files s1.$data_split.gz,  s2.$data_split.gz,
@@ -27,6 +28,8 @@ class NLIDataReader(object):
         examples = []
         id = 0
         for sentence_a, sentence_b, label in zip(s1, s2, labels):
+            label=label.rstrip("\n")
+            logging.info(f"label:{label}")
             guid = "%s-%d" % (filename, id)
             id += 1
             examples.append(InputExample(guid=guid, texts=[sentence_a, sentence_b], label=self.map_label(label.rstrip("\n"))))
@@ -44,7 +47,7 @@ class NLIDataReader(object):
         return len(self.get_labels())
 
     def map_label(self, label):
-        print(f"value of this label is"+label)
+        logging.info(f"value of this label is {label}")
         after_strip=label.strip().lower()
-        print(f"value of after strip label is" + after_strip)
+        logging.info(f"value of after strip label is {after_strip}" )
         return self.get_labels()[after_strip]
