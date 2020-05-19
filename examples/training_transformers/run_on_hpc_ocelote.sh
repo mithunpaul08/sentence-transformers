d33317ec141016061207c93a9840cad68ebf63ba
mithun <mithunpaul08@gmail.com>
2020-05-18 18:49:40 -0700

    commenting out the remove venv part

--- examples/training_transformers/run_on_hpc_ocelote.sh
+++ examples/training_transformers/run_on_hpc_ocelote.sh
@@ -9,19 +9,17 @@
 cd /home/u11/mithunpaul/
 module load cuda90/neuralnet/7/7.3.1.20
 module load python/3.6/3.6.5
-rm -rf my_virtual_env
-mkdir my_virtual_env
-python3 -m venv my_virtual_env
+#rm -rf my_virtual_env
+#mkdir my_virtual_env
+#python3 -m venv my_virtual_env
 source my_virtual_env/bin/activate
-pip install torch==1.5.0+cu92 torchvision==0.6.0+cu92 -f https://download.pytorch.org/whl/torch_stable.html
+#pip install torch==1.5.0+cu92 torchvision==0.6.0+cu92 -f https://download.pytorch.org/whl/torch_stable.html
 
 
 #####my code part
 export PYTHONPATH="/home/u11/mithunpaul/sentence-transformers/"
 cd /home/u11/mithunpaul/sentence-transformers
-pip install -r requirements.txt
-#pip install torch==1.5.0+cu101 torchvision==0.6.0+cu101 -f https://download.pytorch.org/whl/torch_stable.htm
-#pip install torch torchvision
+#pip install -r requirements.txt
 cd /home/u11/mithunpaul/sentence-transformers/examples
 
 #remove these two lines if you want to run on full data
